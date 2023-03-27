bfc7d1710045c57d546670aede9ab1671eabba48
Ümit Şimşek <umitsimsek002@gmail.com>
2023-03-27 17:14:15 +0300

    Initial Commit

---
+++ tryMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tryMovies
+//
+//  Created by Ümit Şimşek on 27.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
